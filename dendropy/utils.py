b5215c97284f33c9ee38eb986ef1ff0482e1fb26
Jeet Sukumaran <jeetsukumaran@gmail.com>
2008-09-26 15:00:10 -0500

    Add pretty_print_delta_time() to utils.py.

--- dendropy/utils.py
+++ dendropy/utils.py
@@ -282,6 +282,10 @@ class OrderedCaselessDict(dict):
                 self[key] = value
         return ocd
     
+def pretty_print_deltatime(deltatime):
+    hours, mins, secs = str(deltatime).split(":")
+    return("%s hour(s), %s minute(s), %s second(s)" % (hours, mins, secs))
+            
 def glob_match(pathname, pattern, respect_case=False, complement=False):
     if respect_case:
         if fnmatch.fnmatchcase(pathname, pattern):
