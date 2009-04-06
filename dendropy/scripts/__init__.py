c8414b62d8fdf7222b538127f758a30f8389a339
Mark T. Holder <mtholder@gmail.com>
2009-04-06 15:20:00 -0500

    scm added to __all__

--- dendropy/scripts/__init__.py
+++ dendropy/scripts/__init__.py
@@ -2,4 +2,6 @@ __all__ = ["cattrees.py",
            "rtreeoutgroup.py",
            "sumtrees.py",
            "treedepth.py",
-           "nexus-to-nexm.py"]
+           "nexus-to-nexm.py",
+           "strict_consensus_merge.py"
+           ]
