5f3ba1a5b364bd3dbf80fb429defc77c95e0ea41
Jeet Sukumaran <jeetsukumaran@gmail.com>
2009-04-28 01:48:33 -0500

    fixed name change bug

--- dendropy/tests/test_coal.py
+++ dendropy/tests/test_coal.py
@@ -40,7 +40,7 @@ class CalcIntervalsTest(unittest.TestCase):
         t = d.trees_from_string("((((a:1, b:1):1, c:2):1, d:3, e:3):2, (f:4, g:4):1)", "newick")[0]
         i1 = coalescent.coalescence_intervals(t)
         assert i1 == [1.0, 1.0, 1.0, 1.0, 1.0], "intervals found = %s" % ", ".join(intervals)
-        i2 = coalescent.num_genes_waiting_times_pairs(t)
+        i2 = coalescent.coalescent_frames(t)
         assert i2 == [(7, 1.0), (6, 1.0), (5, 1.0), (3, 1.0), (2, 1.0)]
         check = coalescent.probability_of_coalescent_tree(t, 10)
 #         check2 = coalescent.debug_coal_prob(t, 10)
