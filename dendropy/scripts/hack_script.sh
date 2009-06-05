4879bb317d848ed3a50973dd085778156063cc26
Mark T. Holder <mtholder@gmail.com>
2009-06-05 11:37:56 -0500

    hacking around to try to make the calls to GARLI batched for improved efficiency

---
+++ dendropy/scripts/hack_script.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+set -x
+
+prevRound=$1
+if ! test -f "${prevRound}"
+then
+	echo "expecting the file ${prevRound} to exist!"
+	exit 1
+fi 
+
+
+if test -z $DENDROPY_SCRIPTS_PAR
+then
+	DENDROPY_SCRIPTS_PAR="${HOME}/Documents/projects/dendropy/dendropy/scripts"
+fi
+
+
+################################################################################
+# add the taxa to the trees from the previous round
+echo 'keep = 5' > add_taxon_commands.txt 
+("${DENDROPY_SCRIPTS_PAR}/igarli_add_tree.py" ${prevRound} >> add_taxon_commands.txt) 2>time_igarli_add_tree.txt || exit
+echo 'quit' >> add_taxon_commands.txt 
+
+echo '#NEXUS' > added_taxa.tre
+echo 'begin trees;' >> added_taxa.tre
+iGarli ../add_garli.conf < add_taxon_commands.txt 2>&1 | grep '\[iGar' >>added_taxa.tre || exit
+echo 'end;' >> added_taxa.tre
+
+
+################################################################################
+# augment the collection of trees
+#	igarli_neighborhood.py will generate no output if it thinks this round is 
+#	done
+round=0
+allTreeFiles='added_taxa.tre'
+cmdFile="neighborhood_command${round}.txt"
+echo 'tmp' > "${cmdFile}"
+while test -s "${cmdFile}"
+do
+	if test $round -eq 0
+	then
+		("${DENDROPY_SCRIPTS_PAR}/igarli_neighborhood.py" 'added_taxa.tre' > "${cmdFile}") 2>time_igarli_neighborhood.txt || exit
+	else
+		("${DENDROPY_SCRIPTS_PAR}/igarli_neighborhood.py" 'added_taxa.tre' "${lastTreeFile}" > "${cmdFile}") 2>>time_igarli_neighborhood.txt || exit
+	fi
+	if test -s "${cmdFile}"
+	then
+		echo 'quit' >> "${cmdFile}"
+		
+		round=`expr $round + 1`
+		lastTreeFile="neighborhood${round}.tre"
+		
+		echo '#NEXUS' > "${lastTreeFile}"
+		echo 'begin trees;' >> "${lastTreeFile}"
+		iGarli ../neighborhood_garli.conf < add_taxon_commands.txt 2>&1 | grep '\[iGar' >> "${lastTreeFile}"  || exit
+		echo 'end;' >> "${lastTreeFile}"
+		allTreeFiles="${allTreeFiles} ${lastTreeFile}"
+	fi
+done
+
+
+################################################################################
+# select the trees to carry to the next round.
+
+(time "${DENDROPY_SCRIPTS_PAR}/igarli_select_trees.py" "${allTreeFiles}" > selected.tre) 2>time_igarli_select_trees.txt
+
+
+
+################################################################################
+# Score the trees for the next round (this makes sure that they have good branch
+#	lengths and gathers the site likeilhoods for the RELL
+########
+echo 'sitelike = 1' > score_commands.txt
+("${DENDROPY_SCRIPTS_PAR}/igarli_add_tree.py" ${prevRound} >> score_commands.txt) 2>>time_igarli_add_tree.txt || exit
+echo 'quit' >> score_commands.txt
+echo '#NEXUS' > incrgarli.tre
+echo 'begin trees;' >> incrgarli.tre
+iGarli ../add_garli.conf < score_commands.txt 2>&1 | grep '\[iGar' >>incrgarli.tre  || exit
+echo 'end;' >> incrgarli.tre
---
+++ dendropy/scripts/igarli_neighborhood.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+
+############################################################################
+##  incr_garli.py
+##
+##  Copyright 2009 Mark T. Holder
+##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License along
+##  with this program. If not, see <http://www.gnu.org/licenses/>.
+##
+############################################################################
+
+
+import sys
+import re
+
+sys.exit(0)
+fn = sys.argv[1]
+f = open(fn, 'rU')
+
+
+#garli_dna_model_pattern = r'\[!GarliModel  r ([.0-9]*) ([.0-9]*) ([.0-9]*) ([.0-9]*) ([.0-9]*) e ([.0-9]*) ([.0-9]*) ([.0-9]*) [.0-9]* a ([.0-9]*) p ([.0-9]*) \]'
+garli_dna_model_pattern = r'\[!GarliModel\s*(r [.0-9]* [.0-9]* [.0-9]* [.0-9]* [.0-9]* e [.0-9]* [.0-9]* [.0-9]* [.0-9]* a [.0-9]* p [.0-9]*)\s*\]'
+garli_tree_model_pat = re.compile(r'\s*Tree\s*tree(\d+)\s*=\s*\[&U\]\[!GarliScore ([-.0-9]*)\]\s*%s\s*(\(.*\))\s*;' % garli_dna_model_pattern)
+garli_tree_pat = re.compile( r'\s*Tree\s*tree(\d+)\s*=\s*\[&U\]\[!GarliScore ([-.0-9]*)\]\s*(\(.*\))\s*;')
+
+
+first = True
+for line in f:
+	m = garli_tree_model_pat.match(line)
+	if m:
+		has_model = True
+	else:
+		m = garli_tree_pat.match(line)
+		has_model = False
+	if m:
+		g = m.groups()
+		result_number = g[0]
+		score = float(g[1])
+		if has_model:
+			sys.stdout.write('model = %s\n' % g[2])
+		sys.stdout.write('tree = %s\n' % g[-1])
+		if first: # this is a way to make 
+			sys.stdout.write('treeNum = 1\n')
+			first = False
+		sys.stdout.write('run\n')
+	elif False:
+		sys.stderr.write("nomatch: %s\n" %line)
+sys.exit(0)
+
