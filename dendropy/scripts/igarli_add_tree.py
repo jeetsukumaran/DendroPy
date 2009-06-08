7a7edecc65cb98d300309def87d801c0538bea78
Mark T. Holder <mtholder@gmail.com>
2009-06-07 23:17:06 -0500

    closer to greatness

--- dendropy/scripts/igarli_add_tree.py
+++ dendropy/scripts/igarli_add_tree.py
@@ -31,7 +31,7 @@ f = open(fn, 'rU')
 #garli_dna_model_pattern = r'\[!GarliModel  r ([.0-9]*) ([.0-9]*) ([.0-9]*) ([.0-9]*) ([.0-9]*) e ([.0-9]*) ([.0-9]*) ([.0-9]*) [.0-9]* a ([.0-9]*) p ([.0-9]*) \]'
 garli_dna_model_pattern = r'\[!GarliModel\s*(r [.0-9]* [.0-9]* [.0-9]* [.0-9]* [.0-9]* e [.0-9]* [.0-9]* [.0-9]* [.0-9]* a [.0-9]* p [.0-9]*)\s*\]'
 garli_after_name_tm_pattern = r'\s*=\s*\[&U\]\[!GarliScore ([-.0-9]*)\]\s*%s\s*(\(.*\))\s*;' % garli_dna_model_pattern
-garli_after_name_tree_pattern = r'\s*=\s*\[&U\]\[!GarliScore ([-.0-9]*)\]\s*(\(.*\))\s*;'
+garli_after_name_tree_pattern = r'\s*=\s*\[.*\]\s*(\(.*\))\s*;'
 tree_prefix = r'\s*Tree\s*tree(\d+)'
 garli_tree_model_pat = re.compile(tree_prefix + garli_after_name_tm_pattern)
 garli_tree_pat = re.compile(tree_prefix + garli_after_name_tree_pattern)
@@ -48,8 +48,8 @@ for line in f:
 	if m:
 		g = m.groups()
 		result_number = g[0]
-		score = float(g[1])
 		if has_model:
+			score = float(g[1])
 			sys.stdout.write('model = %s\n' % g[2])
 		sys.stdout.write('tree = %s\n' % g[-1])
 		if first: # this is a way to make 
