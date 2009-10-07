64ae7f6de40c23df6f85a949c824a3f7bef1cf80
Jeet Sukumaran <jeetsukumaran@gmail.com>
2009-10-07 08:53:13 -0500

    set paup criterion to likelihood

--- dendropy/tests/paup.py
+++ dendropy/tests/paup.py
@@ -396,6 +396,7 @@ def estimate_char_model(char_block,
 
     paup_template = """\
     set warnreset=no;
+    set crit=like;
     exe %(datafile)s;
     lset tratio=estimate rmatrix=estimate nst=%(nst)s basefreq=%(basefreq)s rates=%(rates)s shape=estimate pinvar=%(pinvar)s userbrlens=yes;
     %(tree)s;
