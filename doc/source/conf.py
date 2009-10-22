894c81452ce49c88ad4f77fad9d5188e70488eb2
Jeet Sukumaran <jeetsukumaran@gmail.com>
2009-10-22 17:00:56 -0500

    moving toward using templates

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -96,9 +96,7 @@ rst_epilog = """
 .. _js: http://www.jeetworks.org/about
 .. |mth| replace:: Mark T. Holder
 .. _mth: http://people.ku.edu/~mtholder
-"""
 
-rst_epilog += """
 .. |DendroPy| replace:: DendroPy
 .. _DendroPy: http://packages.python.org/DendroPy/
 .. |dendropy_homepage_url| replace:: http://packages.python.org/DendroPy/
@@ -106,9 +104,7 @@ rst_epilog += """
 .. |dendropy_library_url| replace:: http://packages.python.org/DendroPy/library/index.html
 .. |dendropy_download_url| replace:: http://pypi.python.org/pypi/DendroPy
 .. |dendropy_public_repo_url| replace:: http://github.com/jeetsukumaran/DendroPy
-"""
 
-rst_epilog += """
 .. |Python| replace:: Python
 .. _Python: http://www.python.org/
 .. |Python26| replace:: Python 2.6
@@ -117,16 +113,12 @@ rst_epilog += """
 .. _setuptools: http://pypi.python.org/pypi/setuptools
 .. |Git| replace:: Git
 .. _Git: http://git-scm.com/
-"""
 
-rst_epilog += """
 .. |dendropy_logo| replace:: /_static/dendropy.png
 .. |dendropy_library_doc| replace:: /library/index
 .. |dendropy_tutorial_doc| replace:: /tutorial/index
 .. |sumtrees_doc| replace:: /scripts/sumtrees
-"""
 
-rst_epilog += """
 .. |sumtrees_citation| replace:: Sukumaran, J. and Mark T. Holder. 2009. *SumTrees: Summarization of Split Support on Phylogenetic Trees. Version 1.0.2*. Part of the *DendroPy Phylogenetic Computation Library Version 2.6.1* (|dendropy_homepage_url|).
 .. |dendropy_citation| replace:: Sukumaran, J. and Mark T. Holder. 2009. *DendroPy Phylogenetic Computing Library Version 2.6.1* (|dendropy_homepage_url|).
 .. |dendropy_copyright| replace:: **Copyright 2009 Jeet Sukumaran and Mark T. Holder**
