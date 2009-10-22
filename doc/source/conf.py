fca142c07038421244b30d64b155015fe74cee8d
Jeet Sukumaran <jeetsukumaran@gmail.com>
2009-10-22 08:36:24 -0500

    wrapped theme-ing in conditional for easy build-time switching

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -134,41 +134,46 @@ rst_epilog += """
 
 # -- Options for HTML output ---------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  Major themes that come with
-# Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+if False:
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-# orange =CB9832  green = 81916A,
-html_theme_options = {
-
-    #'bodyfont': '#000000',
-    #'headfont': '#000000',
+    # The theme to use for HTML and HTML Help pages.  Major themes that come with
+    # Sphinx are currently 'default' and 'sphinxdoc'.
+    html_theme = 'sphinxdoc'
     
-    'relbarbgcolor': '#343430', #'#',
-    'relbartextcolor': '#ffffff',
-    'relbarlinkcolor': '#fff000',    
+else:
+    html_theme = 'default'
     
-    'sidebarbgcolor': '#404040',
-    'sidebartextcolor': '#ffffff',
-    'sidebarlinkcolor': '#CB9832',
+    # Theme options are theme-specific and customize the look and feel of a theme
+    # further.  For a list of options available for each theme, see the
+    # documentation.
+    # orange =CB9832  green = 81916A,
+    html_theme_options = {
     
-    'bgcolor': '#eeeedd',
-    'textcolor': '#000000',
-    'linkcolor': '#0000ff',
+        #'bodyfont': '#000000',
+        #'headfont': '#000000',
+        
+        'relbarbgcolor': '#343430', #'#',
+        'relbartextcolor': '#ffffff',
+        'relbarlinkcolor': '#fff000',    
+        
+        'sidebarbgcolor': '#404040',
+        'sidebartextcolor': '#ffffff',
+        'sidebarlinkcolor': '#CB9832',
+        
+        'bgcolor': '#eeeedd',
+        'textcolor': '#000000',
+        'linkcolor': '#0000ff',
+        
+        'headbgcolor': '#ccccbb',
+        'headtextcolor': '#006600',
+        'headlinkcolor': '#fff000',    
     
-    'headbgcolor': '#ccccbb',
-    'headtextcolor': '#006600',
-    'headlinkcolor': '#fff000',    
-
-    'codebgcolor': '#ffffff',
-    'codetextcolor': '#000000',
-    
-    'footerbgcolor': '#000000',
-    'footertextcolor': '#ffffff',    
-}
+        'codebgcolor': '#ffffff',
+        'codetextcolor': '#000000',
+        
+        'footerbgcolor': '#000000',
+        'footertextcolor': '#ffffff',    
+    }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
