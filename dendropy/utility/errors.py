a3b0fa60507da901268c1b2cb38d1231809ddbb1
Jeet Sukumaran <jeetsukumaran@gmail.com>
2009-11-06 03:21:16 -0600

    saved

--- dendropy/utility/errors.py
+++ dendropy/utility/errors.py
@@ -48,3 +48,13 @@ class UnspecifiedFormatError(Exception):
 
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
+
+class MultipleStreamError(Exception):
+
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+class UnspecifiedSourceError(Exception):
+
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
