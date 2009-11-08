bba9c46420a13d5d1c56d4c6e645477cd44d7693
Jeet Sukumaran <jeetsukumaran@gmail.com>
2009-11-08 17:37:00 -0600

    added char array

---
+++ dendropy/test/test_dataobject_create_char_array.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+###############################################################################
+##  DendroPy Phylogenetic Computing Library.
+##
+##  Copyright 2009 Jeet Sukumaran and Mark T. Holder.
+##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License along
+##  with this program. If not, see <http://www.gnu.org/licenses/>.
+##
+###############################################################################
+
+"""
+Tests creation, reading, update, deletion of CharArray objects.
+"""
+
+import unittest
+from cStringIO import StringIO
+from dendropy.utility import error
+from dendropy.test.support import datatest
+from dendropy.test.support import datagen
+import dendropy
+
+class DnaArrayTest(datatest.DataObjectVerificationTestCase):
+
+    def setUp(self):
+        self.char_array1 = datagen.reference_dna_array()
+
+    def testFromCharArray(self):
+        ca2 = dendropy.DnaCharacterArray(self.char_array1)
+        self.assertDistinctButEqual(self.char_array1, ca2, char_type='dna', distinct_taxa=False, equal_oids=False)
+
+if __name__ == "__main__":
+    unittest.main()
