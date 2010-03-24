b1a5e6bd93c1ad2b0cad69f03b33853539e6caa6
Jeet Sukumaran <jeetsukumaran@gmail.com>
2010-03-24 08:23:59 -0500

    fixed num segregating sites

--- dendropy/popgenstat.py
+++ dendropy/popgenstat.py
@@ -87,22 +87,50 @@ def _num_segregating_sites(char_vectors, state_alphabet, ignore_uncertain=True):
     Returns the raw number of segregating sites (polymorphic sites).
     """
     s = 0
-    for vidx, i in enumerate(char_vectors[:-1]):
-        for j in char_vectors[vidx+1:]:
-            if len(i) != len(j):
-                raise Exception("sequences of unequal length")
-            for cidx, c in enumerate(i):
-                c1 = c
-                c2 = j[cidx]
-                if (not ignore_uncertain) \
+    for i, c1 in enumerate(char_vectors[0]):
+        for v in char_vectors[1:]:
+            c2 = v[i]
+            if c1 != c2 \
+                and ((not ignore_uncertain) \
                     or (c1.value is not state_alphabet.gap \
                         and c2.value is not state_alphabet.gap \
                         and len(c1.value.fundamental_ids) == 1 \
-                        and len(c2.value.fundamental_ids) == 1):
-                    if c1.value is not c2.value:
-                        s += 1
-                        continue
+                        and len(c2.value.fundamental_ids) == 1)):
+                s += 1
+                break
     return s
+
+def _tajimas_d(num_sequences, avg_num_pairwise_differences, num_segregating_sites):
+
+    ### VERIFICATION ###
+    ###
+    ### Given: num_sequences = 10, num_pairwise_differences = 3.888889, num_segregating_sites = 16
+    ###  i.e.: tajimas_d(10, 3.888889, 16)  == -1.44617198561
+    ###  Then:    a1 == 2.82896825397
+    ###           a2 == 1.53976773117
+    ###           b1 == 0.407407407407
+    ###           b2 == 0.279012345679
+    ###           c1 == 0.0539216450284
+    ###           c2 == 0.0472267720013
+    ###           e1 == 0.0190605338016
+    ###           e2 == 0.0049489277699
+    ###           D ==  -1.44617198561
+
+    a1 = sum([1.0/i for i in range(1, num_sequences)])
+    a2 = sum([1.0/(i**2) for i in range(1, num_sequences)])
+    b1 = float(num_sequences+1)/(3*(num_sequences-1))
+    b2 = float(2 * ( (num_sequences**2) + num_sequences + 3 )) / (9*num_sequences*(num_sequences-1))
+    c1 = b1 - 1.0/a1
+    c2 = b2 - float(num_sequences+2)/(a1 * num_sequences) + float(a2)/(a1 ** 2)
+    e1 = float(c1) / a1
+    e2 = float(c2) / ( (a1**2) + a2 )
+    D = (
+        float(avg_num_pairwise_differences - (float(num_segregating_sites)/a1))
+        / math.sqrt(
+            (e1 * num_segregating_sites )
+          + ((e2 * num_segregating_sites) * (num_segregating_sites - 1) ))
+        )
+    return D
 
 def num_segregating_sites(char_matrix, ignore_uncertain=True):
     """
@@ -186,16 +214,7 @@ class PopulationPairSummaryStatistics(object):
         self.wakeleys_psi = (float(1)/(a)) * ( ax * (math.sqrt(s2_x)/d_x) + ay * (math.sqrt(s2_y)/d_y) + (2 * n_x * n_y * math.sqrt(s2_xy)/k))
 
         # Tajima's D #
-        a1 = sum([1.0/i for i in range(1,n)])
-        a2 = sum([1.0/(i**2) for i in range(1,n)])
-        b1 = float(n+1)/(3*(n-1))
-        b2 = float(2 * ( (n**2) + n + 3 )) / (9*n*(n-1))
-        c1 = b1 - 1.0/a1
-        c2 = b2 - float(n+2)/(a1 * n) + float(a2)/(a1 ** 2)
-        e1 = float(c1) / a1
-        e2 = float(c2) / ( (a1**2) + a2 )
-        D = float(k - self.num_segregating_sites) / math.sqrt( (e1 * self.num_segregating_sites ) + (e2 * self.num_segregating_sites) * (self.num_segregating_sites - 1) )
-        self.tajimas_d = D
+        self.tajimas_d = _tajimas_d(n, self.average_number_of_pairwise_differences, self.num_segregating_sites)
 
     def _average_number_of_pairwise_differences_between_populations(self):
         """
