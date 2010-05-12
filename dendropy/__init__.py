e6681ed8ccd3d5d5102febad0938039983440569
Jeet Sukumaran <jeetsukumaran@gmail.com>
2010-05-12 17:43:20 -0500

    micro version increment

--- dendropy/__init__.py
+++ dendropy/__init__.py
@@ -41,7 +41,7 @@ from dendropy.dataio import get_reader, get_writer, tree_source_iter, multi_tree
 ## PACKAGE METADATA
 
 __project__ = "DendroPy"
-__version__ = "3.2.2"
+__version__ = "3.2.3"
 
 try:
     try:
