4a4fa2812242226c36c3273683e87319eb442357
Mark T. Holder <mtholder@gmail.com>
2010-11-22 14:18:02 -0600

    bug fix in dispersal matrix production in the biogeodispersal analysis

--- scripts/calculators/geodispersal-analysis.py
+++ scripts/calculators/geodispersal-analysis.py
@@ -8,7 +8,8 @@ from dendropy.utility.error import DataParseError
 _DEBUGGING = True
 _LOG = get_logger('geodispersal')
 verbose = False
-
+gStateNames = None
+col_width = 17
 def write_as_nexus(stream, patterns, label):
     stream.write("\n[!%s ]\n" % label)
     p = patterns[0]
@@ -19,8 +20,11 @@ def write_as_nexus(stream, patterns, label):
     Format datatype=standard symbols="012" ;
     Matrix \n""" % (num_areas, num_chars))
     for area_ind in range(num_areas):
-        name = "area%d" % area_ind
-        padding = ' ' * (10 - len(name))
+        if gStateNames:
+            name = gStateNames[area_ind]
+        else:
+            name = "area%d" % area_ind
+        padding = ' ' * (col_width - len(name))
         stream.write("%s%s" % (name, padding))
         for p in patterns:
             stream.write(" %d" % p[area_ind])
@@ -41,15 +45,19 @@ def vicariance_patterns(node_list, num_areas):
         curr_pat[i] = 1
     patterns.append(curr_pat)
 
+    count = 0
     for node in node_list[1:]:
+        curr_pat = [0]*num_areas
         p = node.parent_node
+        node.biogeo_number = count
         assert(p)
         par_area = p.state_sets[0]
         child_area = node.state_sets[0]
 
         diff = par_area - child_area
-        curr_pat = [0]*num_areas
+        _LOG.debug("count = %d Par = %s       Des = %s    diff = %s" %(count, str(par_area), str(child_area), str(diff)))
         if diff:
+            # range contraction...
             # parent existed in areas that the descendant did not
             twos = set.intersection(child_area, par_area)
             ones = set.symmetric_difference(child_area, par_area)
@@ -59,7 +67,10 @@ def vicariance_patterns(node_list, num_areas):
             ones = set.union(child_area, par_area)
         for i in ones:
             curr_pat[i] = 1
+        _LOG.debug("curr_pat = %s" %(str(curr_pat)))
         patterns.append(curr_pat)
+        count += 1
+
     return patterns
 
 def dispersal_patterns(node_list, num_areas):
@@ -81,8 +92,10 @@ def dispersal_patterns(node_list, num_areas):
         curr_pat = [0]*num_areas
         for i in twos:
             curr_pat[i] = 2
-        for i in par_area:
+        for i in set.intersection(child_area, par_area):
             curr_pat[i] = 1
+        _LOG.debug("dispersal: count = %d Par = %s, Des = %s, twos = %s, pattern= %s" % 
+                    (node.biogeo_number, str(par_area), str(child_area), str(twos), str(curr_pat)))
         patterns.append(curr_pat)
     return patterns
 
@@ -93,6 +106,9 @@ if __name__ == '__main__':
     (options, args) = parser.parse_args()
     if len(args) == 0:
         sys.exit("Expecting a filename as an argument")
+    if len(args) == 2:
+        labels_filename = args[1]
+        gStateNames = [i.strip() for i in open(labels_filename, "rU").readlines()]
 
     char_index = 0
     char_mat_index = 0
@@ -101,7 +117,7 @@ if __name__ == '__main__':
     tree_index = 0
 
     try:
-        for f in args:
+        for f in args[:1]:
             fo = open(f, "rU")
             dataset = DataSet()
             try:
@@ -138,13 +154,16 @@ if __name__ == '__main__':
                     raise ValueError("A tree of more than 2 leaves is required")
                 outgroup = root_children[0]
 
-            node_list = [i for i in tree.postorder_node_iter()]
+            # we get the preorder and reverse twice, rather than the post-order 
+            #   and reverse it once, so that we traverse the edges left to right
+            node_list = [i for i in tree.preorder_node_iter()]
             for nd in node_list:
                 c = nd.child_nodes()
                 if c:
                     if len(c) != 2:
                         raise ValueError("Tree must be fully resolved")
-
+            
+            node_list.reverse()
             fitch_down_pass(node_list, taxa_to_state_set_map=taxon_to_state_indices)
 
 
@@ -152,7 +171,6 @@ if __name__ == '__main__':
 
             root.state_sets = list(outgroup.state_sets)
             fitch_up_pass(node_list, taxa_to_state_set_map=taxon_to_state_indices)
-
 
             node_list.remove(outgroup)
             num_areas = len(state_alphabet.fundamental_states())
