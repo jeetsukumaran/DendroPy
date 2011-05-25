8d81472b09f0d592f5e3684dcb16df1ef9b99f26
Jeet Sukumaran <jeetsukumaran@gmail.com>
2011-05-25 17:43:07 -0500

    testing with ultramteric trees

--- dendropy/test/test_tree_from_splits.py
+++ dendropy/test/test_tree_from_splits.py
@@ -32,7 +32,7 @@ _LOG = get_logger(__name__)
 
 class NexusTreeListReaderTest(datatest.DataObjectVerificationTestCase):
 
-    def testReferenceTreeFileSameTaxa(self):
+    def testReferenceTree(self):
         ref_tree_list = datagen.reference_tree_list()
         t_tree_list = dendropy.TreeList()
         for ref_tree in ref_tree_list:
@@ -41,6 +41,24 @@ class NexusTreeListReaderTest(datatest.DataObjectVerificationTestCase):
             t_tree = treesplit.tree_from_splits(splits=splits,
                     taxon_set=ref_tree_list.taxon_set,
                     is_rooted=ref_tree.is_rooted)
+            self.assertEqual(ref_tree.symmetric_difference(t_tree), 0)
+
+    def testUltrametricTrees(self):
+        tree_files = [
+                "pythonidae.beast.summary.tre",
+                "primates.beast.mcct.medianh.tre"
+                ]
+
+        for tree_file in tree_files:
+            ref_tree = dendropy.Tree.get_from_path(pathmap.tree_source_path(tree_file),
+                    "nexus",
+                    as_rooted=True)
+            treesplit.encode_splits(ref_tree)
+            splits = ref_tree.split_edges.keys()
+            t_tree = treesplit.tree_from_splits(splits=splits,
+                    taxon_set=ref_tree.taxon_set,
+                    is_rooted=ref_tree.is_rooted)
+            treesplit.encode_splits(t_tree)
             self.assertEqual(ref_tree.symmetric_difference(t_tree), 0)
 
 if __name__ == "__main__":
