c72ecad1c5c8a39e01f0041a215f37c7536b5035
Jeet Sukumaran <jeetsukumaran@gmail.com>
2011-05-26 20:20:23 -0500

    extended documentation

---
+++ doc/source/examples/prune_taxa_with_labels.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import dendropy
+
+tree_str = "[&R] ((A, (B, (C, (D, E)))),(F, (G, H)));"
+
+tree = dendropy.Tree.get_from_string(
+        tree_str,
+        "newick")
+
+print("Before:")
+print(tree.as_string('newick'))
+print(tree.as_ascii_plot())
+tree.prune_taxa_with_labels(["A", "C", "G"])
+print("After:")
+print(tree.as_string('newick'))
+print(tree.as_ascii_plot())
+
