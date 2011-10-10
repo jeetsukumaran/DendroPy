4e3890b87ccb0d1b3af9b11f9541b06c37dd6a06
Mark T. Holder <mtholder@gmail.com>
2011-10-10 13:35:10 -0500

    reversing the orientation of split bitmasks in NormalizedBitmaskDict
    Now the first index will always be 1 rather than 0. All tests pass, so this
    is backwards-compatible within dendropy, but this could break client code that
    relies on the numerical values of splits.

--- dendropy/utility/containers.py
+++ dendropy/utility/containers.py
@@ -160,7 +160,7 @@ class NormalizedBitmaskDict(dict):
     """
 
     def normalize(key, mask):
-        if key & 1:
+        if key & 1 == 0:
             return (~key) & mask
         return key
     normalize = staticmethod(normalize)
