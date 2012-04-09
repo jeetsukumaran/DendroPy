9467a652ec2cf09a798c397bb57f529ab40b5882
Jeet Sukumaran <jeetsukumaran@gmail.com>
2012-04-09 13:53:45 -0400

    example fix

--- doc/source/examples/paup_estimate_model.py
+++ doc/source/examples/paup_estimate_model.py
@@ -7,6 +7,7 @@ data = dendropy.DnaCharacterMatrix.get_from_path("pythonidae.nex", "nexus")
 tree = paup.estimate_tree(data,
         tree_est_criterion='nj')
 model = paup.estimate_model(data,
+        tree,
         num_states=2,
         unequal_base_freqs=True,
         gamma_rates=False,
