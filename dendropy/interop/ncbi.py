442dc4e60cf78ca8932879176f2cd01634ee50ab
Jeet Sukumaran <jeetsukumaran@gmail.com>
2012-06-26 19:32:42 -0400

    add genbank record parsing

--- dendropy/interop/ncbi.py
+++ dendropy/interop/ncbi.py
@@ -20,14 +20,15 @@
 Wrappers for interacting with NCBI databases.
 """
 
-from dendropy.utility import messaging
-_LOG = messaging.get_logger(__name__)
-
+import re
 import urllib
 import sys
 import dendropy
-import re
+from dendropy.dataio.xmlparser import ElementTree
 from dendropy.utility.error import DataParseError
+from dendropy.utility import containers
+from dendropy.utility import messaging
+_LOG = messaging.get_logger(__name__)
 
 GB_FASTA_DEFLINE_PATTERN = re.compile(r'^gi\|(\d+)\|(\w+)\|([\w\d]+).(\d+)\|(.*)$')
 
@@ -114,6 +115,261 @@ def relabel_taxa_from_defline(taxon_set,
                 gbnum_in_front=gbnum_in_front,
                 exclude_gbnum=exclude_gbnum)
     return taxon_set
+
+class GenBankReference(object):
+    """
+    A GenBank reference record.
+    """
+    def __init__(self, xml=None):
+        self.number = None
+        self.position = None
+        self.authors = []
+        self.consrtm = None
+        self.title = None
+        self.journal = None
+        self.medline_id = None
+        self.pubmed_id = None
+        self.remark = None
+        self.db_ids = containers.OrderedCaselessDict()
+        if xml is not None:
+            self.parse_xml(xml)
+
+    def parse_xml(self, xml):
+        self.number = xml.findtext("INSDReference_reference")
+        self.position = xml.findtext("INSDReference_position")
+        for authors in xml.findall("INSDReference_authors"):
+            for author_xml in authors.findall("INSDReference_author"):
+                author = author_xml.findtext("INSDAuthor")
+                self.authors.append(author)
+        self.title = xml.findtext("INSDReference_title")
+        self.journal = xml.findtext("INSDReference_journal")
+        for xrefs in xml.findall("INSDReference_xref"):
+            for xref_xml in xrefs.findall("INSDXref"):
+                dbname = xref_xml.findtext("INSDXref_dbname")
+                dbid = xref_xml.findtext("INSDXref_id")
+                self.db_ids[dbname] = dbid
+            self.pubmed_id = xml.findtext("INSDReference_pubmed")
+            self.medline_id = xml.findtext("INSDReference_medline")
+
+class GenBankInterval(object):
+    """
+    A GenBank Interval record.
+    """
+    def __init__(self, xml=None):
+        self.begin = None
+        self.end = None
+        self.accession = None
+        if xml is not None:
+            self.parse_xml(xml)
+
+    def parse_xml(self, xml):
+        self.begin = xml.findtext("INSDInterval_from")
+        self.end = xml.findtext("INSDInterval_to")
+        self.accession = xml.findtext("INSDInterval_accession")
+
+class GenBankQualifier(object):
+    """
+    A GenBank Qualifier record.
+    """
+    def __init__(self, xml=None):
+        self.name = None
+        self.value = None
+        if xml is not None:
+            self.parse_xml(xml)
+
+    def parse_xml(self, xml):
+        self.name = xml.findtext("INSDQualifier_name")
+        self.value = xml.findtext("INSDQualifier_value")
+
+class GenBankQualifiers(list):
+
+    def __init__(self, *args):
+        list.__init__(self, *args)
+
+    def findall(self, name):
+        results = []
+        for a in self:
+            if a.name == name:
+                results.append(a)
+        results = GenBankQualifiers(results)
+        return results
+
+    def find(self, name, default=None):
+        for a in self:
+            if a.name == name:
+                return a
+        return default
+
+    def get_value(self, name, default=None):
+        for a in self:
+            if a.name == name:
+                return a.value
+        return default
+
+class GenBankFeature(object):
+    """
+    A GenBank Feature record.
+    """
+    def __init__(self, xml=None):
+        self.key = None
+        self.location = None
+        self.qualifiers = GenBankQualifiers()
+        self.intervals = []
+        if xml is not None:
+            self.parse_xml(xml)
+
+    def parse_xml(self, xml):
+        self.key = xml.findtext("INSDFeature_key")
+        self.location = xml.findtext("INSDFeature_location")
+        for intervals in xml.findall("INSDFeature_intervals"):
+            for interval_xml in xml.findall("INSDInterval"):
+                interval = GenBankInterval(interval_xml)
+                self.intervals.append(interval)
+        for qualifiers in xml.findall("INSDFeature_quals"):
+            for qualifier_xml in qualifiers.findall("INSDQualifier"):
+                qualifier = GenBankQualifier(qualifier_xml)
+                self.qualifiers.append(qualifier)
+
+class GenBankFeatures(list):
+
+    def __init__(self, *args):
+        list.__init__(self, *args)
+
+    def findall(self, key):
+        results = []
+        for a in self:
+            if a.key == key:
+                results.append(a)
+        results = GenBankFeatures(results)
+        return results
+
+    def find(self, key, default=None):
+        for a in self:
+            if a.key == key:
+                return a
+        return default
+
+    def get_value(self, key, default=None):
+        for a in self:
+            if a.key == key:
+                return a.value
+        return default
+
+class GenBankRecord(object):
+    """
+    A GenBank record.
+    """
+    def __init__(self, xml=None):
+        self.locus = None
+        self.length = None
+        self.moltype = None
+        self.topology = None
+        self.strandedness = None
+        self.division = None
+        self.update_date = None
+        self.create_date = None
+        self.definition = None
+        self.primary_accession = None
+        self.accession_version = None
+        self.other_seq_ids = {}
+        self.source = None
+        self.organism = None
+        self.taxonomy = None
+        self.references = []
+        self.features = GenBankFeatures()
+        self.sequence_text = None
+        if xml is not None:
+            self.parse_xml(xml)
+
+    def _get_accession(self):
+        return self.primary_accession
+    accession = property(_get_accession)
+
+    def _get_gi(self):
+        return self.other_seq_ids.get("gi", None)
+    gi = property(_get_gi)
+
+    def _get_gb(self):
+        return self.other_seq_ids.get("gb", None)
+    gb = property(_get_gb)
+
+    def parse_xml(self, xml):
+        self.locus = xml.findtext("INSDSeq_locus")
+        self.length = xml.findtext("INSDSeq_length")
+        self.moltype = xml.findtext("INSDSeq_moltype")
+        self.topology = xml.findtext("INSDSeq_topology")
+        self.strandedness = xml.findtext("INSDSeq_strandedness")
+        self.division = xml.findtext("INSDSeq_division")
+        self.update_date = xml.findtext("INSDSeq_update-date")
+        self.create_date = xml.findtext("INSDSeq_create-date")
+        self.definition = xml.findtext("INSDSeq_definition")
+        self.primary_accession = xml.findtext("INSDSeq_primary-accession")
+        self.accession_version = xml.findtext("INSDSeq_accession-version")
+        for other_seqids in xml.findall("INSDSeq_other-seqids"):
+            for other_seqid in other_seqids.findall("INSDSeqid"):
+                seqid = other_seqid.text
+                parts = seqid.split("|")
+                if len(parts) == 1:
+                    self.other_seq_ids[""] = seqid
+                else:
+                    self.other_seq_ids[parts[0]] = parts[1]
+        self.source = xml.findtext("INSDSeq_source")
+        self.organism = xml.findtext("INSDSeq_organism")
+        self.taxonomy = xml.findtext("INSDSeq_taxonomy")
+        for references in xml.findall("INSDSeq_references"):
+            for reference_xml in references.findall("INSDReference"):
+                reference = GenBankReference(reference_xml)
+                self.references.append(reference)
+        for features in xml.findall("INSDSeq_feature-table"):
+            for feature_xml in features.findall("INSDFeature"):
+                feature = GenBankFeature(feature_xml)
+                self.features.append(feature)
+        self.sequence_text = xml.findtext("INSDSeq_sequence")
+
+    def compose_fasta_defline(self):
+        return "gi|%s|gb|%s| %s" % (
+                self.gi,
+                self.accession_version,
+                self.definition)
+
+    def compose_taxon_label(self, components=None, separator=" "):
+        label = []
+        if components is None:
+            components = ["accession", "organism"]
+        for component in components:
+            component = str(getattr(self, component))
+            if component is not None:
+                if " " in component and separator != " ":
+                    component = component.replace(" ", separator)
+                label.append(component)
+        return separator.join(label)
+
+    def as_fasta(self,
+            generate_new_label=False,
+            label_components=None,
+            label_component_separator=" "):
+        if generate_new_label:
+            label = self.compose_taxon_label(components=label_components,
+                    separator=label_component_separator)
+        else:
+            label = self.compose_fasta_defline()
+        sequence_text = self.sequence_text.upper()
+        return ">%s\n%s" % (label, sequence_text)
+
+class GenBankXmlParser(object):
+
+    def __init__(self):
+        pass
+
+    def parse_from_stream(self, stream):
+        tree = ElementTree.parse(stream)
+        root = tree.getroot()
+        gb_recs = []
+        for seq_set in root.iter("INSDSet"):
+            for seq in seq_set.iter("INSDSeq"):
+                gb_rec = GenBankRecord(xml=seq)
+                gb_recs.append(gb_rec)
+        return gb_recs
 
 class Entrez(object):
     """
@@ -229,33 +485,30 @@ class Entrez(object):
         results_str = query.read()
         return results_str
 
-    def fetch_gbrecs_as_plaintext_dict(self,
-            db,
-            ids,
-            key_field="accession"):
+    def fetch_gbrecs_as_plaintext_dict(self, db, ids, verify=True):
         db_name = "nucleotide"
         gb_recs_str = self.fetch(db=db, ids=ids, rettype="gb")
         gb_recs_str_list = re.split(r"^//$", gb_recs_str, flags=re.MULTILINE)
         gb_recs_str_list = [gb_rec for gb_rec in gb_recs_str_list
                     if gb_rec.replace("\n", "")]
-        gb_recs_dict = {}
+        accession_recs = {}
+        gi_recs = {}
         for gb_str in gb_recs_str_list:
             if not gb_str:
                 continue
-            try:
-                accession, gi = parse_accession_number_and_gi_from_gb(gb_str)
-            except TypeError:
-                print "---"
-                print gb_str
-                print "---"
-                raise
-            if key_field == "accession":
-                gb_recs_dict[accession] = gb_str
-            elif key_field == "gi":
-                gb_recs_dict[gi] = gb_str
-            else:
-                raise NotImplementedError("Key field '%s' is not supported" % key_field)
-        return gb_recs_dict
+            accession, gi = parse_accession_number_and_gi_from_gb(gb_str)
+            accession_recs[accession] = gb_str
+            gi_recs[str(gi)] = gb_str
+        result = containers.OrderedCaselessDict()
+        for gbid in ids:
+            sgbid = str(gbid)
+            if sgbid in accession_recs:
+                result[gbid] = accession_recs[sgbid]
+            elif sgbid in gi_recs:
+                result[gbid] = gi_recs[sgbid]
+            elif verify:
+                raise Entrez.AccessionFetchError(sgbid)
+        return result
 
     def fetch_nucleotide_accessions(self,
             ids,
