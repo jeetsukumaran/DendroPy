b608ae7f80d94746af44c93bfdbd2ac20b2fa167
Jeet Sukumaran <jeetsukumaran@gmail.com>
2013-03-07 16:59:00 -0500

    generalized architecture for comparisons

--- scripts/calculators/tree-prometry.py
+++ scripts/calculators/tree-prometry.py
@@ -50,39 +50,64 @@ class Profile(object):
                 val = 0.0
         return val
 
+    def euclidean_distance(self, other):
+        distance = 0.0
+        for i in range(max(len(self._profile_data), len(other._profile_data))):
+            di = pow(self.get(i) - other.get(i), 2)
+            distance += di
+        return distance
+
 class ProfileMatrix(object):
 
     def __init__(self, title):
         self.title = title
-        self._profiles = {}
+        self._profiles = []
 
     def add(self, key, profile_data):
-        assert key not in self._profiles
         profile = Profile(key=key,
                 profile_data=profile_data)
-        self._profiles[profile.key] = profile
-
-    def keys(self):
-        keys = sorted(self._profiles.keys())
-        return keys
+        self._profiles.append(profile)
 
     def __len__(self):
         return len(self._profiles)
 
-    def calc(self):
+    def _calc_distances(self, dist_func):
         results = {}
-        keys = self.keys()
-        for idx1, key1 in enumerate(keys[:-1]):
-            for idx2, key2 in enumerate(keys[idx1+1:]):
-                key = (key1, key2)
-                p1 = self._profiles[key1]
-                p2 = self._profiles[key2]
-                distance = 0.0
-                for i in range(max(len(p1), len(p2))):
-                    di = pow(p1.get(i) - p2.get(i), 2)
-                    distance += di
-                results[key] = distance
+        for idx1, profile1 in enumerate(self._profiles[:-1]):
+            for idx2, profile2 in enumerate(self._profiles[idx1+1:]):
+                d = dist_func(profile1, profile2)
+                results[frozenset([profile1, profile2])] = d
         return results
+
+    def calc(self):
+        results = Distances(title=self.title)
+        euclidean_dist_func = lambda x, y: x.euclidean_distance(y)
+        d = self._calc_distances(euclidean_dist_func)
+        results.add("Euclidean.Raw", d)
+        return results
+
+class Distances(object):
+
+    def __init__(self, title):
+        self.title = title
+        self.distance_names = []
+        self.distances = {}
+        self.comparisons = set()
+
+    def add(self, name, distance_dict):
+        """
+            - `name`
+                string describing distance type (e.g., "Raw.Euclidean")
+            - `distance_dict`
+                dictionary with frozen set of Profile objects being compared
+                as keys, and distance value as values
+        """
+        self.distance_names.append(name)
+        self.distances[name] = distance_dict
+        self.comparisons.update(distance_dict.keys())
+
+    def get(self, distance_name, comparison):
+        return self.distances[distance_name][frozenset(comparison)]
 
 def read_trees(
         tree_filepaths,
@@ -147,27 +172,38 @@ def read_trees(
 def report(profile_matrices,
         out,
         include_header_row=True):
-    if include_header_row:
-        row_parts = ["Tree.1", "Tree.2"]
-        for pm in profile_matrices:
-            row_parts.append("{}.Raw.Euclidean".format(pm.title))
-        out.write("\t".join(row_parts))
-        out.write("\n")
-    keys = []
-    results = {}
+    distance_groups = {}
+    distance_group_titles = []
+    distance_names = set()
+    comparisons_set = set()
     for pm in profile_matrices:
-        results[pm] = pm.calc()
-        keys.extend(results[pm].keys())
-    keys = set(sorted(keys))
-    for key in keys:
-        row_parts = [" <-> ".join(key)]
-        for pm in profile_matrices:
-            try:
-                row_parts.append("{}".format(results[pm][key]))
-            except KeyError:
-                row_parts.append("N/A")
+        d = pm.calc()
+        distance_groups[d.title] = d
+        distance_group_titles.append(d.title)
+        distance_names.update(d.distance_names)
+        comparisons_set.update(d.comparisons)
+    distance_group_titles = sorted(distance_group_titles)
+    distance_names = sorted(distance_names)
+    comparisons_list = []
+    for comparison in comparisons_set:
+        comparisons_list.append( sorted(comparison, key=lambda x: x.key) )
+    comparisons_list = sorted(comparisons_list, key=lambda x: x[0].key + x[1].key)
+    if include_header_row:
+        row_parts = ["P1", "P2"]
+        for dist_title in distance_group_titles:
+            for dist_name in distance_names:
+                row_parts.append("{}.{}".format(dist_title, dist_name))
         out.write("\t".join(row_parts))
         out.write("\n")
+    for comparisons in comparisons_list:
+        row_parts = [comparisons[0].key, comparisons[1].key]
+        for dist_title in distance_group_titles:
+            for dist_name in distance_names:
+                distance = distance_groups[dist_title].get(dist_name, comparisons)
+                row_parts.append("{}".format(distance))
+        out.write("\t".join(row_parts))
+        out.write("\n")
+
 
 def main_cli():
 
