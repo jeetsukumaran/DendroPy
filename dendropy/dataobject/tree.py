ceb05a1bf564838f157bf3b6216dfd57bd729589
Jeet Sukumaran <jeetsukumaran@gmail.com>
2014-03-27 01:11:58 -0400

    Revert "HOTFIX 'Tree.is_compatible_with_split()'" on 'master'.
    This reverts commit 49768738dfab70e8d325d4626a1a0e4346b315ea. All
    HOTFIX's for DendroPy 3.x are now on the DendroPy3 branch.

--- dendropy/dataobject/tree.py
+++ dendropy/dataobject/tree.py
@@ -1966,20 +1966,20 @@ class Tree(TaxonSetLinked, iosys.Readable, iosys.Writeable):
 
     def _check_children_for_split_compatibility(self, nd_list, split):
         for nd in nd_list:
-            if treesplit.is_compatible(nd.edge.split_bitmask, split, self.taxon_set.all_taxa_bitmask()):
+            if is_compatible(nd.edge.split, split):
                 # see if nd has all of the leaves that are flagged as 1 in the split of interest
-                if (nd.edge.split_bitmask & split) == split:
+                if (nd.edge.split & split) == split:
                     return nd
             else:
                 return None
         return None
 
     def is_compatible_with_split(self, split):
-        nd = self.seed_node
+        nd = self.root
         while True:
-            if nd.edge.split_bitmask == split:
+            if nd.edge.split == split:
                 return True
-            nd = self._check_children_for_split_compatibility(nd._child_nodes, split)
+            nd = self._check_children_for_split_compatibility(nd.child_nodes, split)
             if nd is None:
                 return False
 
