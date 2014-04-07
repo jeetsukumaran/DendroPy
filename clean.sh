04eeddd86a872473f367ae5e6a05fbcf3f3e6ff0
Jeet Sukumaran <jeetsukumaran@gmail.com>
2014-04-06 21:53:04 -0400

    Added cleaning script

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 rm -rfv $(find . -name "*.pyc")
+rm -rfv $(find . -name "__pycache__")
 rm -rfv 'dendropy/test/output/'*
 rm -rfv 'dendropy/test/coverage/'*
 #rm -rfv DendroPy.egg-info
