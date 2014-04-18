2d7546d2a75caae1bde1d8915341f1be5dcee8ab
Jeet Sukumaran <jeetsukumaran@gmail.com>
2014-04-18 02:12:52 -0400

    Merged modules

--- dendropy/test/test_dataio_newick_tree.py
+++ dendropy/test/test_dataio_newick_tree.py
@@ -254,6 +254,44 @@ class CommentReadingTests(unittest.TestCase):
             for i in range(1,7):
                 self.assertEqual(nd.comments[i], nd.label)
 
+    def test_comment_association(self):
+        tree_strings = [
+                "([a1][a2]a[a3]:[a4]1[a5],[h1][h2][h3]([b1]b[b2]:[b3][b4]2[b5],[g1][g2]([c1]c[c2]:[c3]3[c4][c5],[f1]([d1]d[d2][d3]:[d4]4[d5],[e1]e[e2]:5[e3][e4][e5])[f2]f[f3]:[f4]6[f5])[g3][g4]g:7[g5])[h4]h[h5]:8)[i1][i2]i[i3]:[i4]9[i5];",
+            ]
+        for tree_string in tree_strings:
+            tree = dendropy.Tree.get_from_string(
+                    tree_string,
+                    "newick",
+                    suppress_external_node_taxa=True)
+            for nd in tree:
+                exp_brlen = ord(nd.label[0]) - ord('a') + 1
+                self.assertEqual(nd.edge.length, exp_brlen)
+                self.assertEqual(len(nd.comments), 5)
+                for comment in nd.comments:
+                    self.assertEqual(comment[0], nd.label)
+
+    def test_anonymous_node_comment_association(self):
+        tree_string1 = "[x1]([x2],[x3]([x4],[x5]([x6],[x7],[x8],[x9])[x10])[x11])[x12];"
+        tree_string2 = "[x1](a[x2],[x3]([x4]b,[x5]([x6]c,[x7]d,[x8]e,[x9]f)g[x10])h[x11])i[x12];"
+        tree1 = dendropy.Tree.get_from_string(tree_string1, "newick", suppress_external_node_taxa=True)
+        tree2 = dendropy.Tree.get_from_string(tree_string2, "newick", suppress_external_node_taxa=True)
+        expected_comments = {
+                "a": ["x2",],
+                "b": ["x4",],
+                "c": ["x6",],
+                "d": ["x7",],
+                "e": ["x8",],
+                "f": ["x9",],
+                "g": ["x5", "x10"],
+                "h": ["x3", "x11"],
+                "i": ["x12"],
+                }
+        nodes1 = [nd for nd in tree1]
+        nodes2 = [nd for nd in tree2]
+        for nd1, nd2 in zip(nodes1, nodes2):
+            self.assertEqual(nd2.comments, expected_comments[nd2.label])
+            self.assertEqual(nd1.comments, expected_comments[nd2.label])
+
 
 class CommentMetaDataTests(unittest.TestCase):
     figtree_metadata_str = """[&Tree1=1,Tree2=2, Tree3={1,2,3}](([xxx]A[&A1=1,A2=2,A3={1,2,3},  ,][A]:[A][A]1[A][A],
@@ -325,6 +363,92 @@ class CommentMetaDataTests(unittest.TestCase):
         for idx, nd in enumerate(tree.postorder_node_iter()):
             self.assertEqual(nd.annotations.values_as_dict(), expected[idx])
 
+class NewickTreeTaxonHandlingTest(unittest.TestCase):
+
+    def test_basic_taxa(self):
+        s = "(a1:3.14e-2,(b2:1.2,(c3:0.5,d4:0.7)e5:111)f6:222)g7:333;"
+        tree = dendropy.Tree.get_from_string(s,
+                "newick",
+                suppress_internal_node_taxa=False)
+        expected = {
+                "a1": 3.14e-2,
+                "b2": 1.2,
+                "c3": 0.5,
+                "d4": 0.7,
+                "e5": 111,
+                "f6": 222,
+                "g7": 333,
+                }
+        tns = tree.taxon_namespace
+        self.assertEqual(len(tns), len(expected))
+        labels = set([t.label for t in tns])
+        self.assertEqual(labels, set(expected.keys()))
+        for nd in tree:
+            self.assertEqual(nd.edge.length, expected[nd.taxon.label])
+
+    def test_quoted_underscores(self):
+        s = "('a_1':3.14e-2,('b_2':1.2,('c_3':0.5,'d_4':0.7)'e_5':111)'f_6':222)'g_7':333;"
+        tree = dendropy.Tree.get_from_string(s,
+                "newick",
+                suppress_internal_node_taxa=False)
+        expected = {
+                "a_1": 3.14e-2,
+                "b_2": 1.2,
+                "c_3": 0.5,
+                "d_4": 0.7,
+                "e_5": 111,
+                "f_6": 222,
+                "g_7": 333,
+                }
+        tns = tree.taxon_namespace
+        self.assertEqual(len(tns), len(expected))
+        labels = set([t.label for t in tns])
+        self.assertEqual(labels, set(expected.keys()))
+        for nd in tree:
+            self.assertEqual(nd.edge.length, expected[nd.taxon.label])
+
+    def test_unquoted_underscores(self):
+        s = "(a_1:3.14e-2,(b_2:1.2,(c_3:0.5,d_4:0.7)e_5:111)f_6:222)g_7:333;"
+        tree = dendropy.Tree.get_from_string(s,
+                "newick",
+                suppress_internal_node_taxa=False)
+        expected = {
+                "a 1": 3.14e-2,
+                "b 2": 1.2,
+                "c 3": 0.5,
+                "d 4": 0.7,
+                "e 5": 111,
+                "f 6": 222,
+                "g 7": 333,
+                }
+        tns = tree.taxon_namespace
+        self.assertEqual(len(tns), len(expected))
+        labels = set([t.label for t in tns])
+        self.assertEqual(labels, set(expected.keys()))
+        for nd in tree:
+            self.assertEqual(nd.edge.length, expected[nd.taxon.label])
+
+    def test_unquoted_underscores_preserved(self):
+        s = "(a_1:3.14e-2,(b_2:1.2,(c_3:0.5,d_4:0.7)e_5:111)f_6:222)g_7:333;"
+        tree = dendropy.Tree.get_from_string(s,
+                "newick",
+                suppress_internal_node_taxa=False,
+                preserve_underscores=True)
+        expected = {
+                "a_1": 3.14e-2,
+                "b_2": 1.2,
+                "c_3": 0.5,
+                "d_4": 0.7,
+                "e_5": 111,
+                "f_6": 222,
+                "g_7": 333,
+                }
+        tns = tree.taxon_namespace
+        self.assertEqual(len(tns), len(expected))
+        labels = set([t.label for t in tns])
+        self.assertEqual(labels, set(expected.keys()))
+        for nd in tree:
+            self.assertEqual(nd.edge.length, expected[nd.taxon.label])
 
 if __name__ == "__main__":
     unittest.main()
