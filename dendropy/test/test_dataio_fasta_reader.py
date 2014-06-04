5b8435ada95430e9084cddc87355f42898da367e
Jeet Sukumaran <jeetsukumaran@gmail.com>
2014-06-04 11:20:05 -0400

    More FASTA tests

--- dendropy/test/test_dataio_fasta_reader.py
+++ dendropy/test/test_dataio_fasta_reader.py
@@ -47,6 +47,27 @@ class FastaDnaReaderTestCase(
                 check_column_annotations=False,
                 check_cell_annotations=False)
 
+class FastaRnaReaderTestCase(
+        standard_file_test_chars.RnaTestChecker,
+        dendropytest.ExtendedTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.build()
+
+    def test_basic_fasta(self):
+        src_path = pathmap.char_source_path("standard-test-chars-rna.fasta")
+        self.verify_get_from(
+                matrix_type=dendropy.RnaCharacterMatrix,
+                src_filepath=src_path,
+                schema="fasta",
+                factory_kwargs={},
+                check_taxon_annotations=False,
+                check_matrix_annotations=False,
+                check_sequence_annotations=False,
+                check_column_annotations=False,
+                check_cell_annotations=False)
+
 class FastaProteinReaderTestCase(
         standard_file_test_chars.ProteinTestChecker,
         dendropytest.ExtendedTestCase):
