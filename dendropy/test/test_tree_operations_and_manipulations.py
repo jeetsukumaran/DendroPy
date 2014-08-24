059c70e1fd241a36b61f47e7df3b936e0da37cba
Jeet Sukumaran <jeetsukumaran@gmail.com>
2014-08-24 17:34:45 -0400

    TrunacteTree test

--- dendropy/test/test_tree_operations_and_manipulations.py
+++ dendropy/test/test_tree_operations_and_manipulations.py
@@ -29,6 +29,31 @@ import re
 
 _LOG = messaging.get_logger(__name__)
 
+class TruncateTree(unittest.TestCase):
+
+    def setUp(self):
+        self.trees = dendropy.TreeList.get_from_path(pathmap.tree_source_path("pythonidae.reference-trees.nexus"), "nexus")
+
+    def check_ultrametric_tree(self, tree, dist):
+        self.assertTrue(tree._debug_tree_is_valid())
+        tree.calc_node_root_distances()
+        for nd in tree.leaf_node_iter():
+            self.assertAlmostEqual(nd.root_distance, nd.distance_from_root())
+            self.assertAlmostEqual(dist, nd.root_distance)
+
+    def test_truncate_ultrametric(self):
+        for tree in self.trees:
+            dists = tree.calc_node_root_distances()
+            min_dist, max_dist = tree.minmax_leaf_distance_from_root()
+            trunc_dists = [(max_dist * f) for f in (0.25, 0.5, 0.75, 0.90)]
+            for td in trunc_dists:
+                working = dendropy.Tree(tree)
+                working.truncate_from_root(td)
+                for idx, leaf in enumerate(working.leaf_node_iter()):
+                    if leaf.label is None and leaf.taxon is None:
+                        leaf.taxon = dendropy.Taxon(label="t%s" % (idx+1))
+                self.check_ultrametric_tree(working, td)
+
 class TestTreeLadderization(unittest.TestCase):
 
     def setUp(self):
