e97e80661dd2dd3a085e5a5856ecb3a31a485655
Jeet Sukumaran <jeetsukumaran@gmail.com>
2014-08-24 22:37:31 -0400

    Added init file for interop module

---
+++ dendropy/interop/__init__.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+##############################################################################
+##  DendroPy Phylogenetic Computing Library.
+##
+##  Copyright 2010 Jeet Sukumaran and Mark T. Holder.
+##  All rights reserved.
+##
+##  See "LICENSE.txt" for terms and conditions of usage.
+##
+##  If you use this work or any portion thereof in published work,
+##  please cite it as:
+##
+##     Sukumaran, J. and M. T. Holder. 2010. DendroPy: a Python library
+##     for phylogenetic computing. Bioinformatics 26: 1569-1571.
+##
+##############################################################################
+
+"""
+Wrappers for interacting with other libraries.
+"""
