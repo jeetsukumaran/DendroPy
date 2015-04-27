b8e4e0687b2bdb87953ff56956997bd64523a674
Jeet Sukumaran <jeetsukumaran@gmail.com>
2015-04-26 23:23:46 -0400

    Update documentation

--- doc/source/examples/pure_kingman1.py
+++ doc/source/examples/pure_kingman1.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 
 import dendropy
-from dendropy.calculate import treesim
+from dendropy.simulate import treesim
 
 taxa = dendropy.TaxonNamespace(["z1", "z2", "z3", "z4", "z5", "z6", "z7", "z8"])
-tree = treesim.pure_kingman(
+tree = treesim.pure_kingman_tree(
         taxon_namespace=taxa,
         pop_size=10000)
 print(tree.as_string(schema="newick"))
