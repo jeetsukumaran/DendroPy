a2f8559b6bf384a48f63bd5e96f7576884139b01
Jeet Sukumaran <jeetsukumaran@gmail.com>
2015-09-16 23:51:35 -0400

    Update schema interface template

--- doc/source/schemas/interfaces/phylip_write.py
+++ doc/source/schemas/interfaces/phylip_write.py
@@ -4,6 +4,7 @@ d.write(
         strict=False,
         spaces_to_underscores=False,
         force_unique_taxon_labels=False,
+        suppress_missing_taxa=False,
         ignore_unrecognized_keyword_arguments=False,
         )
 
