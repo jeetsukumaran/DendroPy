8848f662b15797b1af9053b8422000db914b2516
jmenglund <jan.markus.englund@gmail.com>
2016-07-16 17:49:15 +0200

    Add whitespace

--- doc/source/examples/phyml_estimate_model.py
+++ doc/source/examples/phyml_estimate_model.py
@@ -6,9 +6,11 @@ from dendropy.interop import phyml
 data = dendropy.DnaCharacterMatrix.get(
     path="pythonidae.nex",
     schema="nexus")
+
 tree = dendropy.Tree.get(
     path="pythonidae_user_tree.nex",
     schema="nexus")
+
 result = phyml.run_phyml(
     phyml_path="phyml",
     char_matrix=data,
