79a0cd895382da8dafaabeb067f8cae1a05e4fb8
Connor Yang <connoryang2005@gmail.com>
2023-10-31 19:29:26 -0400

    Remove extra comment from test_popgenism.py

--- tests/smoketests/test_popgenism.py
+++ tests/smoketests/test_popgenism.py
@@ -6,4 +6,3 @@ from . import pytestmark
 
 def test_popgenism():
     populations = FragmentedPopulations(1)
-    # populations.generate_sequences("(A,(B,(C,D)));") # input to get_from_string() breaks; rest of file depends on this
