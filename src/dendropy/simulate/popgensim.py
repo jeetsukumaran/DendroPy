dae496fb773407b3a00070508eab317ea86df4d2
Connor Yang <connoryang2005@gmail.com>
2023-11-17 12:02:54 -0500

    Fix typo in newick string formatting

--- src/dendropy/simulate/popgensim.py
+++ src/dendropy/simulate/popgensim.py
@@ -103,7 +103,7 @@ class FragmentedPopulations(object):
         for i in range(self.num_desc_pops):
             tree_data['id'] = i+1
             desc_lineages.append("%(sp)s%(id)d:%(divt)d" % tree_data)
-        tree_string = "(" + (",".join(desc_lineages)) + ("):%d" % 0) #% (self.num_desc_pops * self.desc_pop_size * 10))
+        tree_string = "(" + (",".join(desc_lineages)) + (")%d;" % 0) #% (self.num_desc_pops * self.desc_pop_size * 10))
         self.pop_tree = dendropy.Tree.get_from_string(tree_string, schema="newick")
         return self.pop_tree
 
