bd066bb35775b5daa6e5e66cb58c2def811df820
Connor Yang <connoryang2005@gmail.com>
2023-11-17 13:19:52 -0500

    Remove tests covered by new unit test

--- tests/smoketests/test_processio.py
+++ tests/smoketests/test_processio.py
@@ -1,25 +1,8 @@
-from dendropy.utility.processio import Session, SessionReader
+from dendropy.utility.processio import Session
 import io
 import tempfile
 
 from . import marksmoke as pytestmark
-
-# TODO: https://github.com/jeetsukumaran/DendroPy/issues/179
-# def test_enqueue_stream():
-
-#     with open("file") as file:
-#         #file.readline()
-#         read = SessionReader(file)
-#         read.enqueue_stream()
-
-
-def test_read():
-    with open("file") as file:
-        read = SessionReader(file)
-        res = read.read()
-        
-    assert res is None
-
 
 def test_start():
     read = Session()
