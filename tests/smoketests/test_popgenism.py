23c37ee4350375c643c00dd504be0c347c4706cd
Connor Yang <connoryang2005@gmail.com>
2023-11-17 13:28:41 -0500

    Fix tests

--- tests/smoketests/test_popgenism.py
+++ tests/smoketests/test_popgenism.py
@@ -1,9 +1,28 @@
-from dendropy.simulate.popgensim import FragmentedPopulations
-
+from dendropy.simulate.popgensim import FragmentedPopulations, pop_gen_tree
+import dendropy
 from . import marksmoke as pytestmark
 
 
-def test_popgenism():
+def test_generate_pop_tree():
+    populations = FragmentedPopulations(1)
+
+    res = populations.generate_pop_tree("") 
+    assert isinstance(res, dendropy.Tree)
+
+def test_generate_gene_tree():
+    pop = FragmentedPopulations(1)
+
+    res = pop.generate_gene_tree("")
+    assert isinstance(res, dendropy.Tree)
+
+def test_generate_sequences():
     # TODO: https://github.com/jeetsukumaran/DendroPy/issues/179#issue-1965884280
     # populations = FragmentedPopulations(1)
+    # populations.generate_sequences("2X2") 
     pass
+
+def test_pop_gen_tree():
+    tree = dendropy.Tree()
+    
+    res = pop_gen_tree(tree=tree)
+    assert isinstance(res, dendropy.Tree)
