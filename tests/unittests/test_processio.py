84d6c4072da677e9a187ac9be36c4ab048aff566
Connor Yang <connoryang2005@gmail.com>
2023-12-01 12:18:35 -0500

    Change file variable names

--- tests/unittests/test_processio.py
+++ tests/unittests/test_processio.py
@@ -6,11 +6,11 @@ from dendropy.utility import processio
 
 class SessionTests(unittest.TestCase):
     def test_session(self):
-        with tempfile.NamedTemporaryFile() as file, open(file.name, mode="w") as filew, open(file.name, mode="r") as filer:
-            reader = processio.SessionReader(filer)
+        with tempfile.NamedTemporaryFile() as file, open(file.name, mode="w") as file_write, open(file.name, mode="r") as file_read:
+            reader = processio.SessionReader(file_read)
             for i in range(3):  
-                filew.write(f"asdf{i}\n")
-                filew.flush()
+                file_write.write(f"asdf{i}\n")
+                file_write.flush()
                 res = reader.read()
                 assert res == f"asdf{i}\n"
         
