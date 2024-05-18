c732987b8473984cb55b754fe98201a129e1986d
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-05-18 16:37:22 +0900

    Strip raw directive from RST

--- setup.py
+++ setup.py
@@ -95,7 +95,17 @@ setup(
     },
     license='BSD',
     description="A Python library for phylogenetics and phylogenetic computing: reading, writing, simulation, processing and manipulation of phylogenetic trees (phylogenies) and characters.",
-    long_description=_read(["README.rst"]),
+    long_description=_read(["README.rst"]).replace(
+""".. raw:: html
+
+   <div align="center">
+""", ""
+    ).replace(
+""".. raw:: html
+
+   </div>
+""", ""
+    ),
     # long_description_content_type="text/markdown",
     long_description_content_type="text/x-rst",
     # install_requires=_read_requirements("requirements.txt"),
