ffd818aeb643ec03a24961245401e99ef32c210d
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-08-18 16:43:49 -0400

    bump version v5.0.1 -> v5.0.2

--- src/dendropy/__init__.py
+++ src/dendropy/__init__.py
@@ -105,7 +105,7 @@ from dendropy.legacy import treesum
 ## PACKAGE METADATA
 import collections
 __project__ = "DendroPy"
-__version__ = "5.0.1"
+__version__ = "5.0.2"
 __author__ = "Jeet Sukumaran and Mark T. Holder and Matthew Andres Moreno"
 __copyright__ = "Copyright 2010-2022 Jeet Sukumaran and Mark T. Holder."
 __citation__ = "Moreno, M. A., Sukumaran, J., and M. T. Holder. 2024. DendroPy 5: a mature Python library for phylogenetic computing. arXiv preprint arXiv:2405.14120. https://doi.org/10.48550/arXiv.2405.14120"
